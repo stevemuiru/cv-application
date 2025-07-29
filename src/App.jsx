f6c92ab877beb6d4b95f9fd40a1a43af385b20c5
stephen muiru <stevembugua26@gmail.com>
2025-07-29 16:30:42 +0300

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import {Information} from './information';
-import {Education} from './Education';
-import {Experience} from './Experience'
+import { Information } from "./information";
+import { Education } from "./education";
+import { Experience } from "./experience";
 
 export default function App() {
   return (
-    <div className="app">
-        <h1>Cv Builder</h1>
-        <Information />
-        <Education />
-        <Experience />
+    <div>
+      <Information />
+      <Education />
+      <Experience />
     </div>
-  )
+  );
 }
